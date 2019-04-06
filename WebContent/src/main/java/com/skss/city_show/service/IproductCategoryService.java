48d0a8d9f6fccbce0d78a4e52c66376f5735d051
wdp59 <wdp59@DESKTOP-FCEDVCH.lan>
2019-04-07 03:54:37 +0800

    项目整理

---
+++ WebContent/src/main/java/com/skss/city_show/service/IproductCategoryService.java
@@ -0,0 +1,38 @@
+package com.skss.city_show.service;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Param;
+
+import com.skss.city_show.dto.ProductCategoryExecution;
+import com.skss.city_show.entity.ProductCategory;
+
+/**
+ * @author Administrator   WebContent
+ *商品类别
+ */
+public interface IproductCategoryService {
+
+	/**
+	 * 某个商品下所有商品列表
+	 * @param shopId
+	 * @return List<ProductCategory>
+	 */
+	public List<ProductCategory> queryProductCategoryList(Long shopId);
+	
+	/**
+	 * 批量插入商品类别
+	 * @param productCategoryList
+	 * @return int
+	 */
+	public ProductCategoryExecution batchInsertProductCatagory(List<ProductCategory>  productCategoryList) throws RuntimeException;
+
+	/**
+	 * 删除指定的商品类别
+	 * @param productCategoryId
+	 * @param shopId
+	 * @return
+	 */
+	public ProductCategoryExecution deleteProductCatagory(@Param("productCategoryId") long productCategoryId,@Param("shopId") long shopId) throws RuntimeException;
+
+}
